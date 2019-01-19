9d596dbc1929614f3a5627274da152c64ba798a4
Dan Sosedoff <dan.sosedoff@gmail.com>
2019-01-19 16:44:48 -0600

    Refactor

--- healthcheck.go
+++ healthcheck.go
@@ -23,32 +23,24 @@ func newHealthcheck(endpoint string) *healthcheck {
 func (h *healthcheck) perform() bool {
 	log.Println("starting healthcheck for", h.endpoint)
 
-	attempt := 0
-
-	for {
-		attempt++
-
-		if attempt > h.attempts {
-			log.Println("all healthchecks failed for", h.endpoint)
-			return false
-		}
-
+	for i := 1; i <= h.attempts; i++ {
 		resp, err := http.Get(h.endpoint)
 		if err != nil {
-			log.Printf("[%d/%d] healthcheck failed for %s: %s\n", attempt, h.attempts, h.endpoint, err)
+			log.Printf("[%d/%d] healthcheck failed for %s: %s\n", i, h.attempts, h.endpoint, err)
 			time.Sleep(h.delay)
 			continue
 		}
 		resp.Body.Close()
 
 		if resp.StatusCode != 200 {
-			log.Printf("[%d/%d] healthcheck failed for %s: responded with %v\n", attempt, h.attempts, h.endpoint, resp.StatusCode)
+			log.Printf("[%d/%d] healthcheck failed for %s: responded with %v\n", i, h.attempts, h.endpoint, resp.StatusCode)
 			time.Sleep(h.delay)
 		}
 
-		break
+		log.Printf("[%d/%d] healthcheck passed for %s", i, h.attempts, h.endpoint)
+		return true
 	}
 
-	log.Printf("[%d/%d] healthcheck passed for %s", attempt, h.attempts, h.endpoint)
-	return true
+	log.Println("all healthchecks failed for", h.endpoint)
+	return false
 }
