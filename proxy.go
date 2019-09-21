9140451c5c62310afd21a8f454607c1c118b4c7c
Dan Sosedoff <dan.sosedoff@gmail.com>
2019-09-21 13:40:49 -0500

    Remove custom websocket code, golang reverse proxy supports websockets since 1.12

--- proxy.go
+++ proxy.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"crypto/tls"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"net/http"
@@ -182,83 +180,6 @@ func (p *Proxy) removeTarget(id string) error {
 	return nil
 }
 
-func handleWebsocket(w http.ResponseWriter, r *http.Request) {
-	hj, ok := w.(http.Hijacker)
-	if !ok {
-		log.Println("[ERROR] Not a hijacker")
-		http.Error(w, "not a hijacker", http.StatusInternalServerError)
-		return
-	}
-
-	in, _, err := hj.Hijack()
-	if err != nil {
-		log.Printf("[ERROR] Hijack error for %s. %s", r.URL, err)
-		http.Error(w, "hijack error", http.StatusInternalServerError)
-		return
-	}
-	defer in.Close()
-
-	out, err := net.Dial("tcp", r.URL.Host)
-	if err != nil {
-		log.Printf("[ERROR] WS error for %s. %s", r.URL, err)
-		http.Error(w, "error contacting backend server", http.StatusInternalServerError)
-		return
-	}
-	defer out.Close()
-
-	err = r.Write(out)
-	if err != nil {
-		log.Printf("[ERROR] Error copying request for %s. %s", r.URL, err)
-		http.Error(w, "error copying request", http.StatusInternalServerError)
-		return
-	}
-
-	b := make([]byte, 1024)
-	if err := out.SetReadDeadline(time.Now().Add(time.Second)); err != nil {
-		log.Printf("[ERROR] Error setting read timeout for %s: %s", r.URL, err)
-		http.Error(w, "error setting read timeout", http.StatusInternalServerError)
-		return
-	}
-
-	n, err := out.Read(b)
-	if err != nil {
-		log.Printf("[ERROR] Error reading handshake for %s: %s", r.URL, err)
-		http.Error(w, "error reading handshake", http.StatusInternalServerError)
-		return
-	}
-
-	b = b[:n]
-	if m, err := in.Write(b); err != nil || n != m {
-		log.Printf("[ERROR] Error sending handshake for %s: %s", r.URL, err)
-		http.Error(w, "error sending handshake", http.StatusInternalServerError)
-		return
-	}
-
-	// https://tools.ietf.org/html/rfc6455#section-1.3
-	// The websocket server must respond with HTTP/1.1 101 on successful handshake
-	if !bytes.HasPrefix(b, []byte("HTTP/1.1 101")) {
-		firstLine := strings.SplitN(string(b), "\n", 1)
-		log.Printf("[INFO] Websocket upgrade failed for %s: %s", r.URL, firstLine)
-		http.Error(w, "websocket upgrade failed", http.StatusInternalServerError)
-		return
-	}
-
-	out.SetReadDeadline(time.Time{})
-
-	errc := make(chan error, 2)
-	cp := func(dst io.Writer, src io.Reader) {
-		_, err := io.Copy(dst, src)
-		errc <- err
-	}
-
-	go cp(out, in)
-	go cp(in, out)
-	err = <-errc
-	if err != nil && err != io.EOF {
-		log.Printf("[INFO] WS error for %s. %s", r.URL, err)
-	}
-}
-
 // isValidMethod returns true if given HTTP methid is valid
 func (proxy *Proxy) isValidMethod(method string) bool {
 	return strings.Index(allowedMethods, method) >= 0
@@ -318,7 +239,6 @@ func (proxy *Proxy) startIdleContainersForHost(host string) error {
 func (proxy *Proxy) handleRequest(rw http.ResponseWriter, req *http.Request) {
 	wrapRw := &responseWriter{w: rw}
 
-	// Tweak request host and method
 	req.Host = strings.ToLower(req.Host)
 	req.Method = strings.ToUpper(req.Method)
 
@@ -374,11 +294,10 @@ func (proxy *Proxy) handleRequest(rw http.ResponseWriter, req *http.Request) {
 	// Handle websocket proxy
 	upgrade := req.Header.Get("Upgrade")
 	if upgrade == "websocket" || upgrade == "Websocket" {
-		handleWebsocket(wrapRw, req)
+		proxy.proxy.ServeHTTP(rw, req)
 	} else {
 		// configure HSTS
 		rw.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-
 		proxy.proxy.ServeHTTP(wrapRw, req)
 	}
 
